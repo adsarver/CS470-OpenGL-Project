cb80c02de724e291225aa1c87557296720ae2164
ericgill2528 <ericgill1999@gmail.com>
2024-11-27 20:00:13 -0500

    Tiny Updates For Characters

--- characters.py
+++ characters.py
@@ -5,39 +5,85 @@ from OpenGL.GLU import *
 import pywavefront
 
 
-scene = pywavefront.Wavefront('person-cat.obj', collect_faces=True)
+person_scene = pywavefront.Wavefront('PyWaveObjs\\person.obj', collect_faces=True)
 
-scene_box = (scene.vertices[0], scene.vertices[0])
-for vertex in scene.vertices:
-    min_v = [min(scene_box[0][i], vertex[i]) for i in range(3)]
-    max_v = [max(scene_box[1][i], vertex[i]) for i in range(3)]
-    scene_box = (min_v, max_v)
+person_scene_box = (person_scene.vertices[0], person_scene.vertices[0])
+for vertex in person_scene.vertices:
+    min_v = [min(person_scene_box[0][i], vertex[i]) for i in range(3)]
+    max_v = [max(person_scene_box[1][i], vertex[i]) for i in range(3)]
+    person_scene_box = (min_v, max_v)
 
-scene_size     = [scene_box[1][i]-scene_box[0][i] for i in range(3)]
+scene_size     = [person_scene_box[1][i]-person_scene_box[0][i] for i in range(3)]
 max_scene_size = max(scene_size)
 scaled_size    = 5
 scene_scale    = [scaled_size/max_scene_size for i in range(3)]
-scene_trans    = [-(scene_box[1][i]+scene_box[0][i])/2 for i in range(3)]
+scene_trans    = [-(person_scene_box[1][i]+person_scene_box[0][i])/2 for i in range(3)]
 
-def Model():
+def Person():
+    glPushMatrix()
+    glScalef(*scene_scale)
+    glTranslatef(*scene_trans)
+    glColor3f(1.000, 0.627, 0.478)
+    for mesh in person_scene.mesh_list:
+        glBegin(GL_QUADS)
+        for face in mesh.faces:
+            for vertex_i in face:
+                glVertex3f(*person_scene.vertices[vertex_i])
+        glEnd()
+    glColor3f(1, 1, 1)
+    glPopMatrix()
+
+arm_scene = pywavefront.Wavefront('PyWaveObjs\\PersonArm.obj', collect_faces=True)
+
+arm_scene_box = (arm_scene.vertices[0], arm_scene.vertices[0])
+for vertex in arm_scene.vertices:
+    min_v = [min(arm_scene_box[0][i], vertex[i]) for i in range(3)]
+    max_v = [max(arm_scene_box[1][i], vertex[i]) for i in range(3)]
+    arm_scene_box = (min_v, max_v)
+
+
+def arm():
+    glPushMatrix()
+    glScalef(*scene_scale)
+    glTranslatef(*scene_trans)
+    glColor3f(1.000, 0.627, 0.478)
+    for mesh in arm_scene.mesh_list:
+        glBegin(GL_TRIANGLES)
+        for face in mesh.faces:
+            for vertex_i in face:
+                glVertex3f(*arm_scene.vertices[vertex_i])
+        glEnd()
+    glColor3f(1, 1, 1)
+    glPopMatrix()
+
+cat_scene = pywavefront.Wavefront('PyWaveObjs\\Cat.obj', collect_faces=True)
+
+cat_scene_box = (cat_scene.vertices[0], cat_scene.vertices[0])
+for vertex in cat_scene.vertices:
+    min_v = [min(cat_scene_box[0][i], vertex[i]) for i in range(3)]
+    max_v = [max(cat_scene_box[1][i], vertex[i]) for i in range(3)]
+    cat_scene_box = (min_v, max_v)
+
+
+def cat():
     glPushMatrix()
     glScalef(*scene_scale)
     glTranslatef(*scene_trans)
 
-    for mesh in scene.mesh_list:
+    for mesh in cat_scene.mesh_list:
         glBegin(GL_TRIANGLES)
         for face in mesh.faces:
             for vertex_i in face:
-                glVertex3f(*scene.vertices[vertex_i])
+                glVertex3f(*cat_scene.vertices[vertex_i])
         glEnd()
-
+    glColor3f(1, 1, 1)
     glPopMatrix()
 
 def main():
         pygame.init()
-        display = (800, 600)
+        display = (1080, 720)
         pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-        gluPerspective(45, (display[0] / display[1]), 1, 500.0)
+        gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
         glTranslatef(0.0, 0.0, -10)
 
         while True:
@@ -56,11 +102,12 @@ def main():
                     if event.key == pygame.K_DOWN:
                         glTranslatef(0,-1,0)
 
-            glRotatef(1, 5, 1, 1)
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-            Model()
+            Person()
+            arm()
+            cat()
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
             pygame.display.flip()
