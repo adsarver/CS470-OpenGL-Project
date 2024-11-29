0fc4fd3efe17f4796be0d7cf616c5e2e7f489cac
ericgill2528 <ericgill1999@gmail.com>
2024-11-29 18:42:45 -0500

    Added car from wavefront import

--- characters.py
+++ characters.py
@@ -69,12 +69,57 @@ def cat():
     glPushMatrix()
     glScalef(*scene_scale)
     glTranslatef(*scene_trans)
-
+    glTranslatef(-1, 0, 2.5)
+    glRotatef(1, 0, 1, 0)
     for mesh in cat_scene.mesh_list:
         glBegin(GL_TRIANGLES)
         for face in mesh.faces:
             for vertex_i in face:
                 glVertex3f(*cat_scene.vertices[vertex_i])
+        glEnd()
+    glColor3f(1, 1, 1)
+    glPopMatrix()
+
+Car_scene = pywavefront.Wavefront('PyWaveObjs\\Car.obj', collect_faces=True)
+
+Car_scene_box = (Car_scene.vertices[0], Car_scene.vertices[0])
+for vertex in Car_scene.vertices:
+    min_v = [min(Car_scene_box[0][i], vertex[i]) for i in range(3)]
+    max_v = [max(Car_scene_box[1][i], vertex[i]) for i in range(3)]
+    Car_scene_box = (min_v, max_v)
+
+
+def Car():
+    glPushMatrix()
+    glScalef(*scene_scale)
+    glTranslatef(*scene_trans)
+    for mesh in Car_scene.mesh_list:
+        glBegin(GL_LINES)
+        for face in mesh.faces:
+            for vertex_i in face:
+                glVertex3f(*Car_scene.vertices[vertex_i])
+        glEnd()
+    glColor3f(1, 1, 1)
+    glPopMatrix()
+
+Trunk_scene = pywavefront.Wavefront('PyWaveObjs\\Trunk.obj', collect_faces=True)
+
+Trunk_scene_box = (Trunk_scene.vertices[0], Trunk_scene.vertices[0])
+for vertex in Trunk_scene.vertices:
+    min_v = [min(Trunk_scene_box[0][i], vertex[i]) for i in range(3)]
+    max_v = [max(Trunk_scene_box[1][i], vertex[i]) for i in range(3)]
+    Trunk_scene_box = (min_v, max_v)
+
+
+def Trunk():
+    glPushMatrix()
+    glScalef(*scene_scale)
+    glTranslatef(*scene_trans)
+    for mesh in Trunk_scene.mesh_list:
+        glBegin(GL_QUADS)
+        for face in mesh.faces:
+            for vertex_i in face:
+                glVertex3f(*Trunk_scene.vertices[vertex_i])
         glEnd()
     glColor3f(1, 1, 1)
     glPopMatrix()
@@ -101,13 +146,32 @@ def main():
                         glTranslatef(0,1,0)
                     if event.key == pygame.K_DOWN:
                         glTranslatef(0,-1,0)
+                    if event.key == pygame.K_a:
+                        glTranslatef(0.5, 0, 0)
+                        glPushMatrix()
+                        Person()
+                        glTranslatef(0.5, 0 , 0)
 
+                        glPopMatrix()
+                    if event.key == pygame.K_d:
+                        glRotatef(5, -1, 0, 0)
+                    if event.key == pygame.K_w:
+                        glRotatef(45, 0, 1, 0)
+                    if event.key == pygame.K_s:
+                        glRotatef(45, 0, -1, 0)
+                    if event.key == pygame.K_q:
+                        glRotatef(5, 0, 0, 1)
+                    if event.key == pygame.K_e:
+                        glRotatef(5, 0, 0, -1)
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
             Person()
             arm()
             cat()
+            Car()
+            Trunk()
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
             pygame.display.flip()
